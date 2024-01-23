5a9466797a64467984f002d6d80214ef3b67d179
Cristobal Ruiz <41586818+Crinlorite@users.noreply.github.com>
2024-01-23 16:17:27 +0100

    Update script.js

--- script.js
+++ script.js
@@ -1,11 +1,16 @@
-function calcularEmbarazo() {
-    var fechaConcepcionInput = document.getElementById("fechaConcepcion").value;
-    var fechaConcepcion = new Date(fechaConcepcionInput);
+document.addEventListener('DOMContentLoaded', function () {
+    // Fijar la fecha de concepción al 08/08/2023
+    var fechaConcepcion = new Date('08/08/2023');
 
+    // Obtener la fecha actual
     var fechaActual = new Date();
+
+    // Calcular el número de semanas de embarazo
     var semanasEmbarazo = Math.floor((fechaActual - fechaConcepcion) / (7 * 24 * 60 * 60 * 1000));
+
+    // Calcular el número de días de embarazo
     var diasEmbarazo = Math.floor((fechaActual - fechaConcepcion) / (24 * 60 * 60 * 1000) % 7);
 
-    var resultadoElemento = document.getElementById("resultado");
-    resultadoElemento.innerHTML = "Actualmente estás en la semana " + semanasEmbarazo + " y el día " + diasEmbarazo + " de embarazo";
-}
+    // Mostrar el resultado al usuario
+    alert("Actualmente estás en la semana " + semanasEmbarazo + " y el día " + diasEmbarazo + " de embarazo");
+});
