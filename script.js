14fe4f0d27f89567a748d00404ace7b798690d7e
Cristobal Ruiz <41586818+Crinlorite@users.noreply.github.com>
2024-01-23 16:31:18 +0100

    Update script.js

--- script.js
+++ script.js
@@ -1,17 +1,15 @@
-document.addEventListener('DOMContentLoaded', function () {
-    // Fijar la fecha de concepción al 08/08/2023
-    var fechaConcepcion = new Date('08/08/2023');
+// Fijar la fecha de concepción al 08/08/2023
+var fechaConcepcion = new Date('08/08/2023');
 
-    // Obtener la fecha actual
-    var fechaActual = new Date();
+// Obtener la fecha actual
+var fechaActual = new Date();
 
-    // Calcular el número de semanas de embarazo
-    var semanasEmbarazo = Math.floor((fechaActual - fechaConcepcion) / (7 * 24 * 60 * 60 * 1000));
+// Calcular el número de semanas de embarazo
+var semanasEmbarazo = Math.floor((fechaActual - fechaConcepcion) / (7 * 24 * 60 * 60 * 1000));
 
-    // Calcular el número de días de embarazo
-    var diasEmbarazo = Math.floor((fechaActual - fechaConcepcion) / (24 * 60 * 60 * 1000) % 7);
+// Calcular el número de días de embarazo
+var diasEmbarazo = Math.floor((fechaActual - fechaConcepcion) / (24 * 60 * 60 * 1000) % 7);
 
-    // Mostrar el resultado al usuario
-    var resultadoElemento = document.getElementById("resultado");
-    resultadoElemento.innerHTML = "Actualmente Virginia está en la semana " + semanasEmbarazo + " y el día " + diasEmbarazo + " de embarazo";
-});
+// Mostrar el resultado al usuario
+var resultadoElemento = document.getElementById("resultado");
+resultadoElemento.innerHTML = "Actualmente estás en la semana " + semanasEmbarazo + " y el día " + diasEmbarazo + " de embarazo";
